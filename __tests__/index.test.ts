39b0a80c5f43582ddb2529f207170dca4fe8b6ad
entsuka70 <entsuka70@gmail.com>
2021-02-07 11:22:17 +0900

    feat: multiply()の単体テスト作成

---
+++ __tests__/index.test.ts
@@ -0,0 +1,7 @@
+import { multiply } from "../src/index";
+
+describe('Let us FIRST TIME TDD !!', () => {
+    it('check multiply function', () => {
+        expect(multiply([3, 10, 3])).toBe(90);
+    });
+});
