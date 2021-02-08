aa7c1d8318a5df6a5e4c7e70075206795be13599
entsuka70 <entsuka70@gmail.com>
2021-02-08 09:04:30 +0900

    fix: multiply()の修正
    引数配列:number から 可変長引数:number

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,6 +2,6 @@ import { multiply } from "../src/index";
 
 describe('Let us FIRST TIME TDD !!', () => {
     it('check multiply function', () => {
-        expect(multiply([3, 10, 3])).toBe(90);
+        expect(multiply(3, 10, 3)).toBe(90);
     });
 });
